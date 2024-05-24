6103dacea7becb184dba9063cc1df5018a542b40
pradumkumar6 <pradumjaiswar790@gmail.com>
2024-05-25 01:31:59 +0530

    First-Commit

---
+++ src/main/java/com/pradum/videocallapp/user/User.java
@@ -0,0 +1,13 @@
+package com.pradum.videocallapp.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String status;
+}
